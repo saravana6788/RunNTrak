8a1f3d71e5b044bd610449eece2314653192d0ae
saravana6788 <saravanan.ganapathy@gmail.com>
2024-10-07 23:57:12 -0600

    Created Custom Material Theme Added Required Fonts,Icons and Themes

--- app/src/main/java/com/skcodes/runntrak/MainActivity.kt
+++ app/src/main/java/com/skcodes/runntrak/MainActivity.kt
@@ -11,7 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.skcodes.runntrak.ui.theme.RunNTrakTheme
+import com.skcodes.presentation.designsystem.RunNTrakTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
